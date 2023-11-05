774c18ebfa09c3824dd9de1d230e695d06bc2f9c
drako9159 <chochito9159@gmail.com>
2023-11-05 11:39:42 -0600

    configure project for test spring security 6

---
+++ src/test/java/drako/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package drako.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
